1bdef52e4ba7ce8d8b7d18ab7208d9ee18de7b3b
Sneha Sah <snehasah0307@gmail.com>
2026-09-10 16:43:09 +0530

    update backend configuration

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,14 +3,20 @@ import cors from "cors";
 
 import "dotenv/config";
 
+import fs from"fs";      //file system
+import path from "path";
+
 import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
+
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(cors({origin: FRONTEND_URL, credentials: true }));
@@ -19,6 +25,14 @@ app.use(clerkMiddleware());
 app.get("/health", (req,res)=>{
     res.status(200).json({ ok:true });
 });
+
+if(fs.existsSync(publicDir)){          // means-> does public folder exists
+    app.use(express.static(publicDir))  //by middleware serve the built react files
+
+    app.get("/{*any}",(req,res,next) => {   // any other URL send back index.html
+        res.sendFile(path.join(publicDir,"index.html") , err => next(err));
+    });      
+}
 
 app.listen(PORT, () => {
     connectDB();
