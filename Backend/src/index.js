a8b3dbec6341233d7574fd5d08b76213eda594ea
Sneha Sah <snehasah0307@gmail.com>
2026-09-13 17:02:10 +0530

    Implement authentication routes and middleware for user validation

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,6 +14,7 @@ import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
 import clerkWebhook from "./webhooks/clerk.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
@@ -31,6 +32,8 @@ app.use(clerkMiddleware());
 app.get("/health", (req,res)=>{
     res.status(200).json({ ok:true });
 });
+
+app.use("api/auth" , authRoutes)
 
 if(fs.existsSync(publicDir)){          // means-> does public folder exists
     app.use(express.static(publicDir))  //by middleware serve the built react files
