07207bd1d2f8ec3328c82546361e2d2cb7c6c650
Sneha Sah <snehasah0307@gmail.com>
2026-09-18 15:45:36 +0530

    fix: import PageLoader component in App.jsx for loading state management

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { WallpaperProvider } from "./context/WallpaperContext";
 import { Navigate, Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import AuthPage from "./pages/AuthPage";
+import PageLoader from "./components/PageLoader";
 
 
 
